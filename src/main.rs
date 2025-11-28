d8492cc8ad0a8e7f6d609eec13a1d068772d7d54
devgeminiesefo <devgeminiesefo@gmail.com>
2025-11-28 13:18:28 +0000

    bitwise read function added

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,46 @@ fn toggle(mut register: u16, mut mask: u16) -> u16 {
     }
 }
 
+fn read_bit(register: u16, mut mask: u16) -> u16 {
+    clear_terminal();
+    println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+    loop {
+        let action: String = input("A. Read Bit\nB. Quit\n");
+        clear_terminal();
+        println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+            if (action == "A") | (action == "a") {
+                let bit_index: u32 = input("Read Bit | Bit Index [0 - 15]: ").parse().expect("Expected An Integer: u32");
+                clear_terminal();
+                println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+                if bit_index > 15 {
+                    clear_terminal();
+                    println!("{} -> [{}]\n", "Invalid Index", bit_index);
+                    println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+                    continue;
+                } else {
+                    mask = mask << bit_index;
+                    let mut tmp_register: u16 = register & mask;
+                    tmp_register = tmp_register >> bit_index;
+                    if tmp_register == 1 {
+                        println!("{} -> [1] | [{}]\n", "Bit", "High");
+                    } else if tmp_register == 0 {
+                        println!("{} -> [0] | [{}]\n", "Bit", "Low")
+                    } else {
+                        println!("{} -> [{:#06X}] | [{:#06X}]\n", "ReadError", tmp_register, register);
+                        continue;
+                    }
+                }
+            } else if (action == "B") | (action == "b") | (action == "q") | (action == "Q") {
+                break register;
+            } else {
+                clear_terminal();
+                println!("{} -> [{}]\n", "Invalid Operation", action);
+                println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+                continue;
+            }
+        }
+    }
+
 fn input(prompt: &str) -> String {
     println!("{}", prompt);
     let mut buf: String = String::new();
@@ -79,6 +119,10 @@ fn options(prompt: &str, register: u16) -> String {
             break "set high".to_string();
         } else if (choice == "l") | (choice == "L") {
             break "set low".to_string();
+        } else if (choice == "r") | (choice == "R") {
+            break "read bit".to_string();
+        } else if (choice == "q") | (choice == "Q") {
+            break "quit".to_string();
         } else {
             clear_terminal();
             println!("{} -> [{}]\n", "Invalid Operation", choice);
@@ -94,9 +138,9 @@ fn main() {
 
     clear_terminal();
     println!("{:#018b} | {:#06X} | {}\n", register, register, register);
-    loop {
+    'main: loop {
         let choice: String = options(
-            "[Options]:\n  Toggle: T\n  Set High: H\n  Set Low: L", 
+            "[Options]:\n  Toggle: T\n  Set High: H\n  Set Low: L\n  Read Bit: R\n  Quit: Q\n", 
             register
         );
         register = {
@@ -106,11 +150,19 @@ fn main() {
                 set_high(register, mask)
             } else if choice == "set low" {
                 set_low(register, mask)
-            } else {
+            } else if choice == "read bit" {
+                read_bit(register, mask);
                 register
+            } else if choice == "quit" {
+                break 'main;
+            } else {
+                continue;
             }
         };
         clear_terminal();
         println!("{:#018b} | {:#06X} | {}\n", register, register, register);
     }
+    clear_terminal();
+    println!("{:#018b} | {:#06X} | {}\n", register, register, register);
+    println!("{}", "Register Operations -> Completed!");
 }
